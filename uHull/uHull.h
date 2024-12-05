fb46031660178a772f42b787dcb7fbf00972391a
swainwri <wainwright_steve61@yahoo.com>
2024-12-05 11:39:21 +0700

    Initial Commit

---
+++ uHull/uHull.h
@@ -0,0 +1,18 @@
+//
+//  uHull.h
+//  uHull
+//
+//  Created by Steve Wainwright on 05/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for uHull.
+FOUNDATION_EXPORT double uHullVersionNumber;
+
+//! Project version string for uHull.
+FOUNDATION_EXPORT const unsigned char uHullVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uHull/PublicHeader.h>
+
+
---
+++ uHullTests/uHullTests.swift
@@ -0,0 +1,17 @@
+//
+//  uHullTests.swift
+//  uHullTests
+//
+//  Created by Steve Wainwright on 05/12/2024.
+//
+
+import Testing
+@testable import uHull
+
+struct uHullTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
