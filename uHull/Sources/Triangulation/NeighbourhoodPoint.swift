011e9e35f2a24509442f8655dced46382ed44188
swainwri <wainwright_steve61@yahoo.com>
2024-12-07 15:17:58 +0700

    first commit for uHull swift library & example

---
+++ uHull/Sources/Triangulation/NeighbourhoodPoint.swift
@@ -0,0 +1,9 @@
+//
+//  NeighbourhoodPoint.swift
+//  EZPlotter
+//
+//  Created by Steve Wainwright on 07/03/2021.
+//  Copyright © 2021 Whichtoolface.com. All rights reserved.
+//
+
+import Foundation
--- uHullExampleTests/uHullExampleTests.swift
+++ uHullExampleTests/uHullExampleTests.swift
@@ -1,14 +1,14 @@
 //
-//  uHullTests.swift
-//  uHullTests
+//  uHullExampleTests.swift
+//  uHullExampleTests
 //
 //  Created by Steve Wainwright on 05/12/2024.
 //
 
 import Testing
-@testable import uHull
+@testable import uHullExample
 
-struct uHullTests {
+struct uHullExampleTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
